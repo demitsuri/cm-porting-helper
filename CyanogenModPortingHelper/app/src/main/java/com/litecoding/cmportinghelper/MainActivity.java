590329810f4e9133fffccf2aaf48424952a4d04d
Dmitry Vorobiev <vorobievds@gmail.com>
2015-09-06 15:08:51 +0300

    [-] removed unused action menu [*] fixed gradle script to use proper package name

--- CyanogenModPortingHelper/app/src/main/java/com/litecoding/cmportinghelper/MainActivity.java
+++ CyanogenModPortingHelper/app/src/main/java/com/litecoding/cmportinghelper/MainActivity.java
@@ -112,9 +112,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+		return false;
     }
     
     @Override
